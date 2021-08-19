471321b7689c0a4743125a48f879aae7c0e38225
cook1e <vincent.schweiger@xolley.de>
2021-08-19 16:30:25 +0200

    stuff

--- maker.sh
+++ maker.sh
@@ -22,9 +22,11 @@ sed -i 's/"java": ">=16"/"java": ">=8"/g' $m
 
 # loot_tables
 LT=$L/src/main/resources/data/minecraft/loot_tables
-
+FT=$L/src/main/resources/data/foodmod/loot_tables
 wget https://github.com/legendary-cookie/foodmod-version-compat/raw/master/spruce_leaves.json -O $LT/blocks/spruce_leaves.json
 
 wget https://raw.githubusercontent.com/legendary-cookie/foodmod-version-compat/master/village_plains_house.json -O $LT/chests/village/village_plains_house.json
+
+wget https://raw.githubusercontent.com/legendary-cookie/foodmod-version-compat/master/tomato_crop.json -O $FT/blocks/tomato_crops.json
 
 rm $LT/chests/village/village_house_plains.json
